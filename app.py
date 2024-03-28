3a573f1d1511ed01d29a91394e929174b18a5847
ijaasM4 <98891153+ijaasM4@users.noreply.github.com>
2024-03-28 16:24:14 +0530

    Add

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+from pymongo import MongoClient;
+import re
 app = Flask(__name__)
 CORS(app)
 
+
+# ! Regex
+Email_Regex = r'^[a-zA-Z]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+# ! Routes
 @app.route('/', methods=['GET'])
 def Example():
-    
     return "Welcome";
-
-
-
 
 
 @app.route('/Login', methods=['POST'])
 def Show():
     data = request.json;
-    Username, Email, QuestionID = data['username'], data['email'], data['queid'];
-    print(Username, Email, QuestionID)
-    return jsonify({"message":"success"})
+    Username, Email, QuestionID = str(data['username'], data['email'], data['queid']);
+    if re.match(Email_Regex, Email):
+        print(Username, Email, QuestionID)
+        return jsonify({"message":"success"})
+    else:
+        return jsonify({"error":"Email Error"})
 
 
 
